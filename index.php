ad487a822f8604270e3e6a13dc7145cb8abc5970
polemarch <polemarch@192.168.1.159>
2013-08-05 12:09:59 +0300

    kardesyazilim danışmanlık hizmetleri zeus erp

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
  */
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
+date_default_timezone_set('Europe/Istanbul');
+
+$betik_zd = date_default_timezone_get();
 $q = isset($_GET ['q']) ? $_GET ['q'] : null;
 $q = rtrim($q, '/');
 $q = explode('/', $q);
---
+++ skins/frontend/markado/js/main.js
@@ -0,0 +1,38 @@
+/*
+ *kardesyazilim.net
+ *leo@kardesyazilim
+ *25.Tem.2013-15:47:27 
+ *main
+ */
+
+$(document).ready(function() {
+  //$(".carousel").carousel();
+
+
+ $('.megamenu').megaMenuCompleteSet({
+        menu_speed_show : 300, // Time (in milliseconds) to show a drop down
+        menu_speed_hide : 200, // Time (in milliseconds) to hide a drop down
+        menu_speed_delay : 200, // Time (in milliseconds) before showing a drop down
+        menu_effect : 'hover_fade', // Drop down effect, choose between 'hover_fade', 'hover_slide', etc.
+        menu_click_outside : 1, // Clicks outside the drop down close it (1 = true, 0 = false)
+        menu_show_onload : 0, // Drop down to show on page load (type the number of the drop down, 0 for none)
+        menu_responsive:1 // 1 = Responsive, 0 = Not responsive
+    });
+    /*  Galleria.loadTheme('skins/frontend/markado/js/themes/classic/galleria.classic.min.js');
+
+    // Initialize Galleria
+    Galleria.run('#galleria', {
+        transition: 'fade',
+        imageCrop: true,
+        _toggleInfo: false,
+        autoplay: 7000,
+        thumbnails: false,
+        title: false
+
+    });*/
+    $('.ftm').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 500);
+    });
+    //mcarousel
+    
+});
